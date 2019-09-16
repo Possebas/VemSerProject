2c2a1a112f5a16ee9211c2a03feb5e02037faf53
Alexandre Viegas Betti <alexandre.viegas@dbccompany.com.br>
2019-09-16 16:25:00 -0300

    sprin.io.

---
+++ LandingPageDbc/src/main/java/br/com/dbccompany/LandingPageDbc/LandingPageDbcApplication.java
@@ -0,0 +1,13 @@
+package br.com.dbccompany.LandingPageDbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandingPageDbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandingPageDbcApplication.class, args);
+	}
+
+}
