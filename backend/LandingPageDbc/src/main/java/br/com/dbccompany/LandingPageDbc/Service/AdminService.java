4d8e47332f388216bb56d9bac182cd88df576add
Alexandre Viegas Betti <alexandre.viegas@dbccompany.com.br>
2019-09-19 10:00:30 -0300

    feat(AdminService):md5 na service.

--- backend/LandingPageDbc/src/main/java/br/com/dbccompany/LandingPageDbc/Service/AdminService.java
+++ backend/LandingPageDbc/src/main/java/br/com/dbccompany/LandingPageDbc/Service/AdminService.java
@@ -1,11 +1,25 @@
 package br.com.dbccompany.LandingPageDbc.Service;
 
-import org.springframework.stereotype.Service;
-
 import br.com.dbccompany.LandingPageDbc.Entity.Admin;
 import br.com.dbccompany.LandingPageDbc.Repository.AdminRepository;
+import br.com.dbccompany.LandingPageDbc.Utils.Cryptography;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AdminService extends AbstractService<AdminRepository, Admin> {
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public Admin saveEntity(Admin admin) throws Exception {
+        String password = Cryptography.md5(admin.getPassword());
+        if (password != null){
+            admin.setPassword(password);
+            return super.saveEntity(admin);
+        }else {
+            System.out.println("ERROR CRYPTOGRAPHY!!");
+            return null;
+        }
+    }
 
 }
