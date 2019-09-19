b543d4ec8359318b85d6e2998a4b2fdac51848f8
Murilo Israel Da Silva Rodrigues <murilo.rodrigues@dbccompany.com.br>
2019-09-19 12:12:16 -0300

    refactor(CandidateDetail): refatorar componentes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,13 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 /* Components */
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import CandidateDetail from './pages/CandidateDetail';
 import { PrivateRoute } from './components/PrivateRoute';
 import Register from './pages/Form';
-import CandidateDetail from "./pages/CandidateDetail";
-import {InvalidRoutes} from './pages/InvalidRoutes';
+
 
 /* CSS */
 import "./App.css";
-
 
 export default class App extends Component {
   render() {
@@ -21,9 +20,9 @@ export default class App extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
           <Route path="/candidateDetail" component={CandidateDetail} />
-          <Route path="*" component={InvalidRoutes} />
+          <Route path="/register" component={Register} />
+          <Route path="*" component={() => <h1 className="text-center">Page not found</h1>} />
         </Switch>
       </BrowserRouter>
     );
