b2de50e78b194b1fafe55cf45f06aa13bad5c907
Alexandre Viegas Betti <alexandre.viegas@dbccompany.com.br>
2019-09-20 20:09:11 -0300

    feat(navbar):video navbar e filtro.

--- frontend/src/components/NavBar copy.jsx
+++ frontend/src/components/NavBar copy.jsx
@@ -15,14 +15,11 @@ import {
   MDBBtn
 } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Cover from 'react-video-cover';
-import BlackKeys from '../videos/BlackKeys.webm'
 
 /* Image and css */
 import "../css/navbar.css"
 import background from "../images/fundoDBC.jpg";
 import logoVemSer from '../images/logoVemSer.png';
-import { isAbsolute } from 'path';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -30,7 +27,6 @@ class NavBar extends React.Component {
     this.state = {
       collapse: false,
       isWideEnough: false,
-      resizeNotifier: () =>{},
     };
     this.onClick = this.onClick.bind(this);
   }
@@ -47,19 +43,6 @@ class NavBar extends React.Component {
   }
 
   render() {
-    const videoOptions ={
-      src : 'https://storage.coverr.co/videos/Black_Keys?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjExNDMyN0NEOTRCMUFCMTFERTE3IiwiaWF0IjoxNTY5MDE1MzUxfQ.pCkN9igqxBko73ziO5V3jS3JZTIUJJUXj2EkvP_JRpA',
-      autoPlay: true,
-      muted: true,
-      loop: true,
-    }
-    const style = {
-      width: '100vw',
-      height: '100vh',
-      top: 0,
-      left: 0,
-      zIndex: -1,
-    };
     return (
       <div>
         <header>
@@ -77,7 +60,7 @@ class NavBar extends React.Component {
               {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
               <MDBCollapse isOpen={this.state.collapse} navbar >
                 <MDBNavbarNav right >
-                  <MDBNavItem className="under mr-2 ">
+                  <MDBNavItem className="under mr-2">
                     <MDBNavLink href="#whatis" to="#whatis" >O que é?</MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem className="under mr-2">
@@ -105,23 +88,12 @@ class NavBar extends React.Component {
               </MDBCollapse>
             </MDBNavbar>
           </Router>
-          <MDBView className="dbc">
-            <div className="videoCover" >
-              <Cover 
-              videoOptions = {videoOptions}
-              remeasureOnWindowresize
-              getResizeNotifier = {resizeNotifier => {
-                this.state({
-                  resizeNotifier,
-                });
-              }}
-              />
-            </div>  
-              <MDBContainer className="black-text text-center textVideo" id="tt">
-                  <h1 className=" font-weight-bold" id="tituloVS">Vem Ser DBC</h1> 
-                  <p className="" id="titleNav"> Curte <span id="tecnologia">tecnologia</span> ?  <br/> Gosta de resolver <span id="desafios">desafios </span> ?  <br/> Quer aprender a <span id="desenvolver">desenvolver</span> ? <br/> Não fique de fora dessa! </p>            
-                  <MDBBtn className="amber darken-4 mb-5 mt-5 button"><a href="/register" className="black-text">INSCREVA-SE</a></MDBBtn>
-              </MDBContainer>
+          <MDBView src={background}>
+            <MDBContainer className="text-center" id="tt">
+              <h1 className="white-text font-weight-bold" id="tituloVS">Vem Ser DBC</h1> 
+              <p className="" id="titleNav"> Curte <span id="tecnologia">tecnologia</span> ?  <br/> Gosta de resolver <span id="desafios">desafios </span> ?  <br/> Quer aprender a <span id="desenvolver">desenvolver</span> ? <br/> Não fique de fora dessa! </p>            
+              <MDBBtn className="amber darken-4 mt-5"><a href="/register" className="black-text">INSCREVA-SE</a></MDBBtn>
+            </MDBContainer>
           </MDBView>
           
         </header>
