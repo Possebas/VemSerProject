b3cf416d59b42450b55eab08293c1aa1bebeea71
Gustavo Possebon Machado <gustavo.possebon@dbccompany.com.br>
2019-09-20 14:38:52 -0300

    fix(frontend): ajustar nomes de componentes

---
+++ frontend/src/components/home/Carrousel/CarouselTchnology.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { MDBCarousel, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer } from
+"mdbreact";
+
+import docker from "../../../images/carouselTechnology/docker.jpg";
+import intellij from "../../../images/carouselTechnology/intellij.png";
+import reactJS from "../../../images/carouselTechnology/ReactJS.png";
+import springBoot from "../../../images/carouselTechnology/springBoot.png";
+import visualStudio from "../../../images/carouselTechnology/visualStudio.png";
+import js from "../../../images/carouselTechnology/js.jpeg";
+import '../../../css/carousel.css';
+
+const CarouselTechnology = () => {
+  return (
+    <MDBContainer >
+      <MDBCarousel
+        activeItem={5}
+        length={6}
+        showControls={true}
+        showIndicators={false}
+        className=" "
+        slide
+      >
+        <MDBCarouselInner className="imgcarousel">
+          <MDBCarouselItem itemId="1">
+            <MDBView >
+              <img
+                className="d-block w-100 imageo"
+                src={springBoot}
+                alt="First slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="2">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src={reactJS}
+                alt="Second slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="3">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src={visualStudio}
+                alt="Third slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="4">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src={intellij}
+                alt="Third slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="5">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src={docker}
+                alt="Third slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="6">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src={js}
+                alt="Third slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+        </MDBCarouselInner>
+      </MDBCarousel>
+    </MDBContainer>
+  );
+}
+
+export default CarouselTechnology;
--- frontend/src/components/home/WhatIs.jsx
+++ frontend/src/components/home/WhatIs.jsx
@@ -12,7 +12,7 @@ export const WhatIs = () => {
                 <h1 className="text-center mt-5 font-weight-bold">O que é?</h1>
                 <MDBContainer className="w-100 h-100 d-inline-block pt-5">
                     <MDBRow lg="6" md="12" sm="12">
-                        <MDBCol lg="6" md="12" sm="12" className="mx-auto mt-3">
+                        <MDBCol lg="6" md="12" sm="12" className="mx-auto mt-3 my-auto">
                             <p className="text-center pt-5 purple-text" id="text-comentarios">
                                 O Programa <span>Vem Ser DBC</span> é uma experiência única onde jovens são selecionados para participar do programa de capacitação, onde irão conhecer ferramentas e linguagens que são utilizadas no dia a dia dos profissionais de T.I.  
                                 </p>
