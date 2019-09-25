87294373079eddf273d6e54a8182c40fb87e82d2
Murilo Israel da Silva Rodrigues <murilo.rodrigues@dbccompany.com.br>
2019-09-24 22:08:46 -0300

    refactor(frontend/QuestionsCandidate): requisição de infoCandidatos.

--- frontend/src/components/QuestionsCandidate.jsx
+++ frontend/src/components/QuestionsCandidate.jsx
@@ -6,16 +6,17 @@ export default class QuestionsCandidate extends Component{
   constructor(props){
     super(props);
     this.state = { 
-      visible: false
+      visible: false,
+      detailOfCandidate: {}
     };
   };
 
   showModal = () => {
     axios.get(`http://localhost:8080/api/question/`)
     .then(respUsuario => {
-        console.log("resp",respUsuario.data);
         this.setState({
-          visible: true
+          visible: true,
+          detailOfCandidate: respUsuario.data
         });
     });
   };
@@ -53,6 +54,31 @@ export default class QuestionsCandidate extends Component{
           {this.props.candidate.address.complement} -
           {this.props.candidate.address.neighborhood.name}<br/>
           {this.props.candidate.address.neighborhood.city.name}
+          
+          {console.log(this.state.detailOfCandidate)}
+
+          {//this.state.detailOfCandidate.map((item) =>{
+             // if(item.cpf === this.props.candidate.cpf) {
+               // return(
+                 // <>
+                  // Questionário: 
+                  //  {item.detailOfCandidate.availabilityAfterTraining}
+                  //  {item.detailOfCandidate.college}
+                  //  {item.detailOfCandidate.course}
+                  //  {item.detailOfCandidate.dayShifys}
+                  //  {item.detailOfCandidate.educationalInstitution}
+                  //  {item.detailOfCandidate.hasAvailability}
+                  //  {item.detailOfCandidate.logicalKnowledge}
+                  //  {item.detailOfCandidate.otherReason}
+                  //  {item.detailOfCandidate.participated}
+                  //  {item.detailOfCandidate.reasonsForInterest}
+                  //  {item.detailOfCandidate.referencesThatInspire}
+                  //  {item.detailOfCandidate.whatMotivates}
+                  //</>
+              //);}
+            //})
+          }
+
         </Modal>
       </div>
     );
